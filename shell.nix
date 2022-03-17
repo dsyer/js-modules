ffa5393b9b3beb2bf40e6de28dfb9e7a8a6f22c7
Dave Syer <dsyer@vmware.com>
2022-03-17 09:29:03 +0000

    Attempt to make things work in nix-shell

--- shell.nix
+++ shell.nix
@@ -4,11 +4,16 @@ mkShell {
 
   name = "env";
   buildInputs = [
-    figlet nodejs python3
+    figlet nodejs python3 google-chrome-dev firefox-bin
   ];
 
-  postShellHook = ''
-    figlet ":yeah, baby!:"
+  shellHook = ''
+    export PLAYWRIGHT_SKIP_VALIDATE_HOST_REQUIREMENTS=true
+    git clone https://github.com/ludios/nixos-playwright /tmp/nixos-playwright || echo Already cloned
+    (cd /tmp/nixos-playwright; git pull --rebase)
+    npm install -D playwright
+    npx playwright install
+    /tmp/nixos-playwright/fix-playwright-browsers
   '';
 
 }
